9d4fda9de5eacaf38c9a2e593e86d362b16a7e8b
Meccc2760 <chensh2025@shanghaitech.edu.cn>
2026-09-02 15:30:24 +0000

    check0: geturl

--- apps/webget.cc
+++ apps/webget.cc
@@ -9,8 +9,25 @@ using namespace std;
 
 void get_URL( const string& host, const string& path )
 {
-  cerr << "Function called: get_URL(" << host << ", " << path << ")\n";
-  cerr << "Warning: get_URL() has not been implemented yet.\n";
+  // cerr << "Function called: get_URL(" << host << ", " << path << ")\n";
+  // cerr << "Warning: get_URL() has not been implemented yet.\n";
+  TCPSocket tcpsocket;
+  Address address(host, "http");
+  tcpsocket.connect(address);
+  
+  std::string request = 
+    "GET " + path + " HTTP/1.1\r\n" +
+    "Host: " + host + "\r\n" +
+    "Connection: close\r\n" +
+    "\r\n";
+  
+  tcpsocket.write(request);
+
+  std::string response;
+  while(not tcpsocket.eof()){
+    tcpsocket.read(response);
+    cout << response << endl;
+  }
 }
 
 int main( int argc, char* argv[] )
