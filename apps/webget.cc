f135da7ff8a7f42213fdf69cff8089c1e886637c
Meccc2760 <chensh2025@shanghaitech.edu.cn>
2026-09-02 16:19:06 +0000

    check0: webget_complete

--- apps/webget.cc
+++ apps/webget.cc
@@ -14,19 +14,18 @@ void get_URL( const string& host, const string& path )
   TCPSocket tcpsocket;
   Address address(host, "http");
   tcpsocket.connect(address);
-  
+
   std::string request = 
     "GET " + path + " HTTP/1.1\r\n" +
     "Host: " + host + "\r\n" +
     "Connection: close\r\n" +
     "\r\n";
-  
   tcpsocket.write(request);
-
+  
   std::string response;
   while(not tcpsocket.eof()){
     tcpsocket.read(response);
-    cout << response << endl;
+    cout << response;
   }
 }
 
