cdcbad1875a2bcb8f4ad08730201e3e756c8559d
Meccc2760 <chensh2025@shanghaitech.edu.cn>
2026-09-17 09:01:37 +0000

    check2: wrap32

--- src/wrapping_integers.cc
+++ src/wrapping_integers.cc
@@ -6,13 +6,19 @@ using namespace std;
 Wrap32 Wrap32::wrap( uint64_t n, Wrap32 zero_point )
 {
   // Your code here.
-  debug( "unimplemented wrap( {}, {} ) called", n, zero_point.raw_value_ );
-  return Wrap32 { 0 };
+  return Wrap32 { zero_point + (n & 0xFFFFFFFF) };
 }
 
 uint64_t Wrap32::unwrap( Wrap32 zero_point, uint64_t checkpoint ) const
 {
-  // Your code here.
-  debug( "unimplemented unwrap( {}, {} ) called", zero_point.raw_value_, checkpoint );
-  return {};
+  uint64_t iter = checkpoint >> 32;
+  uint32_t offset = this->raw_value_ - zero_point.raw_value_;
+  uint64_t seqno = (iter << 32) + offset;
+
+  if (seqno + (1ULL << 31) < checkpoint)
+    seqno += (1ULL << 32);
+  else if (seqno > checkpoint + (1ULL << 31) && seqno >= (1ULL << 32))
+    seqno -= (1ULL << 32);
+
+  return seqno;
 }
