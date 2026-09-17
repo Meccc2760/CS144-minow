4d88095543ca8b3ba9240b3ce07a88436b1d28de
Meccc2760 <chensh2025@shanghaitech.edu.cn>
2026-09-17 09:02:18 +0000

    check2: receiver

--- src/tcp_receiver.cc
+++ src/tcp_receiver.cc
@@ -6,13 +6,27 @@ using namespace std;
 void TCPReceiver::receive( TCPSenderMessage message )
 {
   // Your code here.
-  debug( "unimplemented receive() called" );
-  (void)message;
+  if(message.RST){
+    this->reader().set_error(); 
+  }
+  if(!this->isn_.has_value()){
+    if(message.SYN){
+      this->isn_ = message.seqno;
+    }else{
+      return;
+    }
+  }
+
+  uint64_t checkpoint = this->writer().bytes_pushed();
+  uint64_t abs_seqno = message.seqno.unwrap(this->isn_.value(), checkpoint);
+  uint64_t stream_idx = abs_seqno+ message.SYN - 1;
+
+  std::string data = message.payload;
+  this->reassembler_.insert(stream_idx, data, message.FIN);
 }
 
 TCPReceiverMessage TCPReceiver::send() const
 {
   // Your code here.
-  debug( "unimplemented send() called" );
   return {};
 }
