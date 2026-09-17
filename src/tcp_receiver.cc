cb3da8763d1f9f4d3cbd48cd04e02baae4375280
Meccc2760 <chensh2025@shanghaitech.edu.cn>
2026-09-17 10:42:28 +0000

    check2: sender

--- src/tcp_receiver.cc
+++ src/tcp_receiver.cc
@@ -10,23 +10,29 @@ void TCPReceiver::receive( TCPSenderMessage message )
     this->reader().set_error(); 
   }
   if(!this->isn_.has_value()){
-    if(message.SYN){
-      this->isn_ = message.seqno;
-    }else{
+    if(!message.SYN){
       return;
     }
+    this->isn_ = message.seqno;
   }
 
   uint64_t checkpoint = this->writer().bytes_pushed();
   uint64_t abs_seqno = message.seqno.unwrap(this->isn_.value(), checkpoint);
   uint64_t stream_idx = abs_seqno+ message.SYN - 1;
 
-  std::string data = message.payload;
-  this->reassembler_.insert(stream_idx, data, message.FIN);
+  this->reassembler_.insert(stream_idx, message.payload, message.FIN);
 }
 
 TCPReceiverMessage TCPReceiver::send() const
 {
   // Your code here.
-  return {};
+  uint16_t window_size = this->writer().available_capacity() > UINT16_MAX ? UINT16_MAX : this->writer().available_capacity();
+  std::optional<Wrap32> ackno;
+  if(this->isn_.has_value()){
+    ackno = this->isn_.value()+this->writer().bytes_pushed() + 1;
+    if(this->writer().is_closed()){
+      ackno = ackno.value() + 1;
+    }
+  }
+  return {ackno, window_size, this->writer().has_error()};
 }
